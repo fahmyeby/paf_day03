7147c51870b20e6669768ec8611e1dd0506ed551
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2025-01-08 08:57:12 +0800

    push

---
+++ src/main/java/com/example/paf_03class/Paf03classApplication.java
@@ -0,0 +1,13 @@
+package com.example.paf_03class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf03classApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf03classApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paf_03class/Paf03classApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paf_03class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf03classApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
